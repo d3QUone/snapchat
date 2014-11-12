866942b961c9166bd993d75a06e41b1b473c8794
Владимир <volkvid@yandex.ru>
2014-11-12 22:31:38 +0300

    Update main.py
    errors in login snapchat

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import urllib2, socket
-#import pysnap
+import requests
+from snapi import Snapchat
 
 inputData = []
 result = []
 
+# start new thread with a pack of users
 def main():
     global result
     
@@ -13,7 +15,6 @@ def main():
     except BaseException as ex:
         print "No file named 'proxies.txt', error " + str(ex)
         print "\nPress any key to exit"
-        # add safe exit
         raw_input()
         exit()
 
@@ -22,17 +23,37 @@ def main():
         data = inputFile.read().split("\r\n")
         for item in data:
             item = item.split(":")
-            inputData.append({"user":item[0], "pass":item[1]})
-        
-        #print inputData
+            inputData.append({"user":item[0].split("@")[0], "pass":item[1]})
     except BaseException as ex:
         print "No file named 'input.txt', error " + str(ex)
         print "\nPress any key to exit"
-        # add safe exit
         raw_input()
         exit()
-    
+
+    image = ""
+    threads = ""
+    try:
+        settingsFile = open("settings.txt", "r")
+        settings = settingsFile.read().split("\n")
+        #print settings
+        for item in settings:
+            if item.find("image:") != -1:
+                image = item.replace(" ", "")
+            if item.find("threads:") != -1:
+                threads = item.replace(" ", "")
+        image = image.split("image:")[1]
+        threads = int(threads.split("threads:")[1])
+        #print "image =", image
+        #print "threads =", threads
+    except BaseException as ex:
+        print "No file named 'settings.txt', error " + str(ex)
+        print "\nPress any key to exit"
+        raw_input()
+        exit()
+
+    #print "\nPress any key to continue"
     #raw_input()
+    #print ""
     
     i = 0
     # +parse proxies 
@@ -42,14 +63,11 @@ def main():
         # +check if ok
         if not is_bad_proxy(proxy):
             print "+ " + proxy
-
             for auth in inputData:
-                
                 process(proxy, auth)
             
         else:
             print "- " + proxy
-
 
 
         #---for tests only
@@ -57,20 +75,29 @@ def main():
         if i > 5:
             break
 
-
     #save results
 
 
 def process(prox, auth):
     global result
-    # http prox
-    print "\nused proxy =" + prox
     print "user="+auth["user"]+", pass="+auth["pass"]
 
     num = 0
+    prox = {"http": "http://"+prox}
+    print "proxies =", prox
 
-    # if 
+    #login here
+    #try:
+    s = Snapchat()
+    s.proxies = prox
+    s.login(auth["user"], auth["pass"])
 
+    friendlist = s.get_friends()
+    print friendlist
+    #except Exception as ex:
+        #print "error user " + str(ex)
+
+    #get list of friends
 
 
 def is_bad_proxy(pip):
@@ -83,14 +110,10 @@ def is_bad_proxy(pip):
         req=urllib2.Request('http://www.google.com')  # change the url address here
         sock=urllib2.urlopen(req)
     except urllib2.HTTPError, e:        
-        #print 'Error code: ', e.code
-        #return e.code
         return True 
     except Exception, detail:
-        #print "ERROR:", detail
         return True
     return False
-
 
 
 
